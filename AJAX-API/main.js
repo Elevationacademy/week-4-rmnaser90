272c293dad9852b7c963c90e78d802e84c696bb2
rmnaser90 <rmnaser90@gmail.com>
2020-11-10 15:51:30 +0200

    gif api extention done

--- AJAX-API/main.js
+++ AJAX-API/main.js
@@ -82,21 +82,25 @@
 
 
 
-// Exercise 4
+// Exercise 4, 5, and extension
 
-$('button').click(function name(params) {
+$('.button').click(function name(params) {
     let giphy=$('input').val()
 
     $.ajax({
 
         method: 'GET',
-        url: `http://api.giphy.com/v1/gifs/search?q=${giphy}&api_key=VErPmAEgchSowqKOrRJh4HXFyS5q1m0b&limit=20`,
+        url: `http://api.giphy.com/v1/gifs/search?q=${giphy}&api_key=VErPmAEgchSowqKOrRJh4HXFyS5q1m0b&limit=5`,
         success: function (response) {
-            $('div').empty()
+            const map1 = response.data.map(x => x.embed_url);
+            console.log(map1);
+            $('#div').empty()
             for (const gif of response.data) {
-                $('div').append($(`
+                $('#div').append($(`
+            <div>
             <iframe src="${gif.embed_url}" frameborder="0"></iframe>
-        
+            <button class="btn" data-favorite=false>add</button>
+            </div>
             `))
         }
        console.log(response);
@@ -110,4 +114,16 @@ $('button').click(function name(params) {
 
 })
 
+let favoriteGifs =[]
+
+$('#div').on('click','.btn',function () {
+    if ($(this).data('favorite')==false) {
+        $(this).data('favorite',true)
+        const url=$(this).closest('div').find('iframe').attr('src')
+        favoriteGifs.push({url})
+        console.log(favoriteGifs);
+    }else{console.log("it's already added");}
+    
+
+})
 
