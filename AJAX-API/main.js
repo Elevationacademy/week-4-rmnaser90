b34fb267a9aa0e57e473e89bf099aaf4842bbe31
rmnaser90 <rmnaser90@gmail.com>
2020-11-10 15:17:48 +0200

    exercise 4 done

--- AJAX-API/main.js
+++ AJAX-API/main.js
@@ -46,29 +46,68 @@
 // fetchBookByTitle(book)
 
 
-//Exercise 2
+// //Exercise 2 and 3
 
-let book = "physics of the future"
-let ISBN = 9782806269171
+// let book = "physics of the future"
+// let ISBN = 9782806269171
 
-const useData = function (response) {
-    console.log(response);
-}
+// const useData = function (response) {
+// // console.log(response);
+// for (const book of response.items) {
 
-const fetchBook = function (queryType, queryValue) {
-    if (queryType == 'title') {
-        queryValue = queryValue.split(' ').join('%20')
-    }
-    const url = `https://www.googleapis.com/books/v1/volumes?q=${queryValue}`
+//    let text = `title: ${book.volumeInfo.title}
+// Author: ${book.volumeInfo.authors[0]} 
+// ISBN: ${book.volumeInfo.industryIdentifiers[0].identifier}   `
+// console.log(text);
+// }    
+// }
+
+// const fetchBook = function (queryType, queryValue) {
+//     if (queryType == 'title') {
+//         queryValue = queryValue.split(' ').join('%20')
+//     }
+//     const url = `https://www.googleapis.com/books/v1/volumes?q=${queryValue}`
+//     $.ajax({
+//         method: "GET",
+//         url: url,
+//         success: useData,
+//         error: function (xhr, text, error) {
+//             console.log(xhr);
+//             console.log(text);
+//             console.log(error);
+//         }
+//     })
+// }
+// fetchBook('title',book)
+
+
+
+// Exercise 4
+
+$('button').click(function name(params) {
+    let giphy=$('input').val()
+
     $.ajax({
-        method: "GET",
-        url: url,
-        success: useData,
-        error: function (xhr, text, error) {
-            console.log(xhr);
-            console.log(text);
-            console.log(error);
+
+        method: 'GET',
+        url: `http://api.giphy.com/v1/gifs/search?q=${giphy}&api_key=VErPmAEgchSowqKOrRJh4HXFyS5q1m0b&limit=20`,
+        success: function (response) {
+            $('div').empty()
+            for (const gif of response.data) {
+                $('div').append($(`
+            <iframe src="${gif.embed_url}" frameborder="0"></iframe>
+        
+            `))
         }
+       console.log(response);
+        },
+        error: function (xhr, text, error) {
+            console.log('error');
+        }
+    
     })
-}
-fetchBook('ISBN',ISBN)
+
+
+})
+
+
